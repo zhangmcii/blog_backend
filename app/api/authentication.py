21baf8f288bbe0bc221157e0f7d8e3c2a4027c32
zmc_li <1912592745@qq.com>
2024-12-23 23:34:34 +0800

    1.api接口对/posts请求放行

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,7 +1,15 @@
 from . import api
 from flask_jwt_extended import jwt_required
+from flask import request
+import re
+
+skip_post_pattern = r'^/api/v1/posts/.*$'
+
 
 @api.before_request
-@jwt_required()
+@jwt_required(optional=True)
 def auth():
-    pass        
+    # 对/api/v1/posts开头的请求放行
+    if re.match(skip_post_pattern, request.path):
+        return
+    return '401'
