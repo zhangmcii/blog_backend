9ddbedf71395e49102d228a6789d8dfaa0b5d0e3
zmc_li <1912592745@qq.com>
2025-01-10 00:55:36 +0800

    1.修复 对指定url开放白名单，其他的url则需要验证jwt

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,15 +1,18 @@
 from . import api
 from flask_jwt_extended import jwt_required
-from flask import request
+from flask import request, jsonify
 import re
 
 skip_post_pattern = r'^/api/v1/posts/.*$'
 
 
 @api.before_request
-@jwt_required(optional=True)
 def auth():
-    # 对/api/v1/posts开头的请求放行
-    if re.match(skip_post_pattern, request.path):
-        return
-    return '401'
+    try:
+        # 对/api/v1/posts开头的请求放行
+        if re.match(skip_post_pattern, request.path):
+            return
+        else:
+            jwt_required()
+    except Exception as e:
+        return jsonify({'message': 'Unauthorized'}), 401
