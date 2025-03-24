3c8759e7ef192070a0322e80c935c23df0d63758
zmc_li <1912592745@qq.com>
2025-03-24 22:08:36 +0800

    1.增强解析客户端请求的逻辑

--- app/decorators.py
+++ app/decorators.py
@@ -1,11 +1,12 @@
 from functools import wraps
-from xmlrpc.client import DateTime
-
 from flask import abort, request
 from flask_jwt_extended import current_user
 from .models import Permission, Log
 from . import db
 from .utils.time_util import DateUtils
+import requests
+import os
+from user_agents import parse
 
 
 def permission_required(permission):
@@ -32,13 +33,13 @@ def log_operate(f):
 
         # 用户身份信息
         is_register = True if current_user else False
-        username = current_user.username if is_register else '游客'
-        client_ip = request.remote_addr
+        client_info = get_client_info()
+        client_info.update({'username': current_user.username if is_register else '游客', 'operate': '访问首页'})
 
         # 构建查询条件
-        log_filter = {'username': username}
+        log_filter = {'username': client_info.get('username')}
         if not is_register:
-            log_filter = {'ip': client_ip}
+            log_filter = {'ip': client_info.get('ip')}
 
         # 获取最近访问记录
         last_visit = Log.query.filter_by(**log_filter).order_by(Log.operate_time.desc()).first()
@@ -50,9 +51,47 @@ def log_operate(f):
 
         # 记录日志
         if should_log:
-            db.session.add(Log(username=current_user.username if current_user else '游客', operate='访问首页',
-                               ip=request.remote_addr))
+            db.session.add(Log(**client_info))
             db.session.commit()
         return f(*args, **kwargs)
 
     return decorate
+
+
+def get_client_info():
+    if 'X-Forwarded-For' in request.headers:
+        client_ip = request.headers.get('X-Forwarded-For')
+    else:
+        client_ip = request.remote_addr
+
+    # 获取用户地理位置
+    if os.environ.get('FLASK_DEBUG', None):
+        country = None
+        city = None
+    else:
+        try:
+            r = requests.get(f'https://ipapi.co/{client_ip}/json/')
+            local_data = r.json()
+            country = local_data.get('country_name')
+            city = local_data.get('city')
+        except Exception:
+            country = None
+            city = None
+
+    # 获取设备信息
+    user_agent = request.headers.get('user-agent')
+    ua = parse(user_agent)
+    user_info = {
+        'browser': ua.browser.family,
+        'browser_version': ua.browser.version_string,
+        'os': ua.os.family,
+        'os_version': ua.os.version_string,
+        'device': ua.device.family,
+
+        'ip': client_ip,
+        'country': country,
+        'city': city
+    }
+    if os.environ.get('FLASK_DEBUG', None):
+        print('44', user_info)
+    return user_info
